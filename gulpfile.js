3a3ec46979e4257123bc020593d6d6bf9da94ec2
野瀬友岐 <karl.vanc0uver1st@gmail.com>
2021-05-14 10:34:42 +0900

    [update]gulpfile.js
    sassを完成させる

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 const { src, dest } = require('gulp');
 const Fiber = require('fibers');
 const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssdeclsort = require('css-declaration-sorter');
 
 sass.compiler = require('sass'); // dart sassを使う
 
 const compileSass = (done) => {
-  src('./src/scss/**/*.scss')
+  const postcssPlugins = [
+    autoprefixer({
+      cascade: false,
+    }),
+    cssdeclsort({ order: 'alphabetical' })
+  ];
+  src('./src/scss/**/*.scss', { sourcemaps: true })
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(sass({
       fiber: Fiber,
       outputStyle: 'expanded'
     }))
-    .pipe(dest('./dist/css'));
+    .pipe(postcss([autoprefixer(
+      {
+        grid: "autoplace",
+        cascade: false
+      }
+    )]))
+    .pipe(postcss([cssdeclsort({ order: 'alphabetical' })]))
+    .pipe(dest('./dist/css', { sourcemaps: './sourcemaps' }));
   done();
 };
 
