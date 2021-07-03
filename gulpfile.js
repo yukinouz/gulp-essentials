9455a4cf6a42d2af9215f51f5a96b4f3474ae955
yukinouz <47273687+yukinouz@users.noreply.github.com>
2021-07-03 15:15:09 +0900

    gulp-sass 5.0.0に更新する

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const { src, dest } = require('gulp');
-const Fiber = require('fibers');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const postcss = require('gulp-postcss');
@@ -8,8 +7,6 @@ const autoprefixer = require('autoprefixer');
 const cssdeclsort = require('css-declaration-sorter');
 const gcmq = require('gulp-group-css-media-queries');
 const mode = require('gulp-mode')();
-
-sass.compiler = require('sass'); // dart sassを使う
 
 const compileSass = (done) => {
   const postcssPlugins = [
@@ -23,10 +20,7 @@ const compileSass = (done) => {
     .pipe(
       plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
     )
-    .pipe(sass({
-      fiber: Fiber,
-      outputStyle: 'expanded'
-    }))
+    .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(postcss(postcssPlugins))
     .pipe(mode.production(gcmq()))
     .pipe(dest('./dist/css', { sourcemaps: './sourcemaps' }));
