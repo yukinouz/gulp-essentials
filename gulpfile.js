216fbb17ba7351dbdebf9737d9adeab7ec1eba80
yukinouz <47273687+yukinouz@users.noreply.github.com>
2021-07-03 14:55:06 +0900

    [rm]sass.compiler
    不要になったので削除

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-sass.compiler = require('sass'); // dart sassを使う
-
 const compileSass = (done) => {
   src('./src/scss/**/*.scss')
     .pipe(sass({
