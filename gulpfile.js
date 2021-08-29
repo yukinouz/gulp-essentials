73e281f580376a3e2405f0b2ae1fb2e5629b3a04
yukinouz <47273687+yukinouz@users.noreply.github.com>
2021-08-29 17:03:10 +0900

    [fix]doubleに統一

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-const { src, dest, watch, series, parallel }  = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssdeclsort = require('css-declaration-sorter');
-const gcmq = require('gulp-group-css-media-queries');
-const mode = require('gulp-mode')();
-const browserSync = require('browser-sync');
-const pug = require('gulp-pug');
-const crypto = require('crypto');
-const hash = crypto.randomBytes(8).toString('hex');
-const replace = require('gulp-replace');
+const { src, dest, watch, series, parallel }  = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssdeclsort = require("css-declaration-sorter");
+const gcmq = require("gulp-group-css-media-queries");
+const mode = require("gulp-mode")();
+const browserSync = require("browser-sync");
+const pug = require("gulp-pug");
+const crypto = require("crypto");
+const hash = crypto.randomBytes(8).toString("hex");
+const replace = require("gulp-replace");
 const tinypng = require("gulp-tinypng-extended");
 const webp = require("gulp-webp");
 
@@ -21,8 +21,8 @@ const webpack = require("webpack");
 const webpackConfig = require("./webpack.config");
 
 const bundleJs = () => {
-  return webpackStream(webpackConfig, webpack).on('error', function (e) {
-    this.emit('end');
+  return webpackStream(webpackConfig, webpack).on("error", function (e) {
+    this.emit("end");
   })
   .pipe(dest("dist/js"));
 };
@@ -33,16 +33,16 @@ const compileSass = (done) => {
       grid: "autoplace",
       cascade: false,
     }),
-    cssdeclsort({ order: 'alphabetical' })
+    cssdeclsort({ order: "alphabetical" })
   ];
-  src('./src/scss/**/*.scss', { sourcemaps: true })
+  src("./src/scss/**/*.scss", { sourcemaps: true })
     .pipe(
-      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
     )
-    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(sass({ outputStyle: "expanded" }))
     .pipe(postcss(postcssPlugins))
     .pipe(mode.production(gcmq()))
-    .pipe(dest('./dist/css', { sourcemaps: './sourcemaps' }));
+    .pipe(dest("./dist/css", { sourcemaps: "./sourcemaps" }));
   done();
 };
 
@@ -51,7 +51,7 @@ const buildServer = (done) => {
     port: 8080,
     files: ["**/*"],
     // 静的サイト
-    server: { baseDir: './dist' },
+    server: { baseDir: "./dist" },
     // 動的サイト
     // proxy: "http://localsite.local/",
     open: true,
@@ -68,12 +68,12 @@ const browserReload = done => {
 };
 
 const compilePug = done => {
-  src(['./src/pug/**/*.pug', '!' + './src/pug/**/_*.pug'])
-    .pipe(plumber(({ errorHandler: notify.onError('Error: <%= error.message %>') })))
+  src(["./src/pug/**/*.pug", "!" + "./src/pug/**/_*.pug"])
+    .pipe(plumber(({ errorHandler: notify.onError("Error: <%= error.message %>") })))
     .pipe(pug({
       pretty: true
     }))
-    .pipe(dest('./dist'));
+    .pipe(dest("./dist"));
   done();
 };
 
@@ -105,18 +105,18 @@ const copyImages = (done) => {
 };
 
 const cacheBusting = done => {
-  src('./dist/index.html')
-    .pipe(replace(/\.(js|css)\?ver/g, '.$1?ver=' + hash))
-    .pipe(replace(/\.(webp|jpg|jpeg|png|svg|gif)/g, '.$1?ver=' + hash))
-    .pipe(dest('./dist'));
+  src("./dist/index.html")
+    .pipe(replace(/\.(js|css)\?ver/g, ".$1?ver=" + hash))
+    .pipe(replace(/\.(webp|jpg|jpeg|png|svg|gif)/g, ".$1?ver=" + hash))
+    .pipe(dest("./dist"));
   done();
 };
 
 const watchFiles = () => {
-  watch( './src/scss/**/*.scss', series(compileSass, browserReload))
-  watch( './src/pug/**/*.pug', series(compilePug, browserReload))
-  watch( './src/js/**/*.js', series(bundleJs, browserReload))
-  // watch('./**/*.html', browserReload)
+  watch( "./src/scss/**/*.scss", series(compileSass, browserReload))
+  watch( "./src/pug/**/*.pug", series(compilePug, browserReload))
+  watch( "./src/js/**/*.js", series(bundleJs, browserReload))
+  // watch("./**/*.html", browserReload)
 };
 
 module.exports = {
